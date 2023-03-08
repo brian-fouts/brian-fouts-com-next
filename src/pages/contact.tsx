0f87a4c2cd78e60dbac7f6a74018ab51c9fbc76f
Brian Fouts <brian@brian-fouts.com>
2023-03-07 22:24:56 -0800

    toast

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,10 @@
+import toast from 'react-hot-toast';
+
 export default function Contact({ }) {
     return (
         <div className="page">
             <h1>Contact</h1>
+            <button onClick={() => toast.success('Success')}>Submit</button>
         </div>
     )
   }
