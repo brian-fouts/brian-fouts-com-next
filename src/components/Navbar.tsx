cb15f692272c2515dce2e27680985e01059a3ec4
Brian Fouts <brian@brian-fouts.com>
2023-03-09 23:35:53 -0800

    hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,62 @@
 import Link from 'next/link'
+import { slide as Menu } from 'react-burger-menu'
+import { useState, useEffect } from 'react';
 
 interface NavbarProps {
 
 };
 
 function HamburgerMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const onOpen = () => {
+        setMenuOpen(true);
+    }
+    const onClose = () => {
+        setMenuOpen(false);
+    }
+    const onNavigate = () => {
+        onClose();
+    }
+
     return (
         <div className="hamburger-menu">
-            <img src="/images/hamburger-menu.svg" alt="Menu" />
+            <Menu
+                isOpen={menuOpen}
+                onOpen={onOpen}
+                onClose={onClose}
+                width="200px"
+                customBurgerIcon={ <img src="/images/hamburger-menu.svg" /> }
+            >
+                <ul>
+                    <li>
+                        <Link href="/" legacyBehavior>
+                            <a onClick={() => onNavigate()}>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/services" legacyBehavior>
+                            <a onClick={() => onNavigate()}>Services</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/demos" legacyBehavior>
+                            <a onClick={() => onNavigate()}>Demos</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" legacyBehavior>
+                            <a onClick={() => onNavigate()}>About</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" legacyBehavior>
+                            <a onClick={() => onNavigate()}>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </Menu>
+            
         </div>
     )
 }
